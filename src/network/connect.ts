4ad66ed53eba99eba3842d29c19647e2cb2ebbae
Tushar Ojha <tusharojha2001@gmail.com>
2024-04-30 00:48:19 +0530

    hide rpc warnings

--- src/network/connect.ts
+++ src/network/connect.ts
@@ -6,7 +6,7 @@ export const connect = async (network?: NetworkConfig): Promise<ApiPromise> => {
   const rpc = network?.rpc || testnet.rpc
 
   const wsProvider = new WsProvider(`wss://${rpc}`);
-  const api = await ApiPromise.create({ provider: wsProvider });
+  const api = await ApiPromise.create({ provider: wsProvider, noInitWarn: true });
 
   return api
 }
